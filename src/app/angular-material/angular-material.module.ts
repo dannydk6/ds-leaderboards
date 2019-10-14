d7f301ba23bf98a659ca119eb819c314777a478c
Tom Fobear <tom@bovsistudios.com>
2019-10-14 19:24:04 -0400

    added material and updated template

---
+++ src/app/angular-material/angular-material.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MatToolbarModule
+  ],
+  exports: [
+    MatToolbarModule
+  ]
+})
+export class AngularMaterialModule { }
